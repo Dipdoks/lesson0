a64f87aaecc5d83c4856ac66eb910c2d22be4d38
Dipdoks <dipdoks@gmail.com>
2024-10-12 19:22:47 +0400

    Практическое задание по теме "Переменные"
    Практическое задание по теме "Переменные"

---
+++ 001.py
@@ -0,0 +1,9 @@
+Number_of_completed_DZ = 12
+print('Number of completed DZ:',Number_of_completed_DZ)
+Number_of_hours_spent = 1.5
+print('Number of hours spent:',Number_of_hours_spent)
+Course_name = 'Python'
+print('Course name:',Course_name)
+Time_for_one_task = Number_of_hours_spent/Number_of_completed_DZ
+print('Time for one task:',Time_for_one_task)
+print('Course name:',Course_name+',','number of completed DZ:',str(Number_of_completed_DZ)+',','number of hours spent:',str(Number_of_hours_spent)+',','time for one task:',str(Time_for_one_task)+" hour's.")
