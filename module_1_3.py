e0b067c481d68d4053d4de6bf92a6bd659598fea
Dipdoks <dipdoks@gmail.com>
2024-10-12 16:17:27 +0400

    Практическая работа
    Практическая работа по уроку "Динамическая типизация"

---
+++ module_1_3.py
@@ -0,0 +1,17 @@
+name = 'Aleksandr'
+print(name)
+age = 35
+print(age)
+age = age+1
+print(age)
+is_student = True
+print(is_student)
+# ниже сделал как в примере:
+name = 'Aleksandr'
+print('Name: '+ (name))
+age = 35
+print(('Age:'),age)
+age = age+1
+print(('New Age:'),age)
+is_student = True
+print(('Is Student:'),is_student)
